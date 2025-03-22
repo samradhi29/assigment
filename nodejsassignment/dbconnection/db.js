98976bc923596f239d6e3dd0d2b860b056563154
samradhi29 <samradhi4320@gmail.com>
2025-03-22 22:18:37 +0530

    Update db.js

--- nodejsassignment/dbconnection/db.js
+++ nodejsassignment/dbconnection/db.js
@@ -14,7 +14,7 @@ db.connect((err)=>{
 });
 
 const createTableQuery = `
-CREATE TABLE IF NOT EXISTS schools (
+CREATE TABLE IF NOT EXISTS school (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     address VARCHAR(255) NOT NULL,
